8fe6653e6d20f7ac830bf2b559686e46d1e67a98
asinbow.wang <asinbow.wang@saybot.com>
2011-10-13 16:19:47 +0800

    fix bug

--- source/dynamic_web/server/app.rb
+++ source/dynamic_web/server/app.rb
@@ -4,19 +4,17 @@ class App < Sinatra::Base
   set :root, SERVER_ROOT_PATH
   set :port, SERVER_CONFIG['port']
 
-  class << self
 
-    def refresh
-      uri = URI.parse(SERVER_CONFIG['tenacy_url'])
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      @new_tenacy_url = response["Location"]
-    end
+  def refresh
+    uri = URI.parse(SERVER_CONFIG['tenacy_url'])
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @new_tenacy_url = response["Location"]
+  end
 
-    def get_redirected_url
-      @new_tenacy_url ||= refresh
-    end
+  def get_redirected_url
+    @new_tenacy_url ||= refresh
   end
 
   get '/' do
