78cd02266850ef23d9dae016c5a46aa514392153
asinbow.wang <asinbow.wang@saybot.com>
2011-11-07 11:01:27 +0800

    enhance redirect

--- source/dynamic_web/server/app.rb
+++ source/dynamic_web/server/app.rb
@@ -4,13 +4,46 @@ class App < Sinatra::Base
   set :root, SERVER_ROOT_PATH
   set :port, SERVER_CONFIG['port']
 
+  def url_file
+    @url_file ||= File.join(ROOT_PATH, SERVER_CONFIG['url_file'])
+  end
+
+  def url_history
+    @url_history ||= begin
+                       return YAML.load(File.read(url_file)) if File.exist?(url_file)
+                       [SERVER_CONFIG['tenacy_url']]
+                     end
+  end
+
+  def try_url_history(history)
+    history.each do |url|
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      case response.code
+      when '200'
+        [false, url]
+      when '301'
+        [true, response["Location"]]
+      when '404'
+        # next
+      else
+        # unknown code, next
+      end
+    end
+  end
 
   def refresh
-    uri = URI.parse(SERVER_CONFIG['tenacy_url'])
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    @new_tenacy_url = response["Location"]
+    history = url_history
+    redirect, url = try_url_history(history)
+    if redirect
+      history.push(url)
+      File.open(url_file, 'w+') do |f|
+        f.write(history.to_yaml)
+      end
+    end
+    url
   end
 
   def get_redirected_url
